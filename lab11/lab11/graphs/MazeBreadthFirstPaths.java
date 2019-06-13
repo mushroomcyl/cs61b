5c1d4f10259b1831be75e71852cde5acd7f334fe
Yonglin Chen <yonglin_chen@foxmail.com>
2019-06-13 11:29:45 -0700

    finish lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import java.util.Queue;
+import java.util.ArrayDeque;
+import java.lang.Integer;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +13,63 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+    private int INFINITY = Integer.MAX_VALUE;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        // TODO: Your code here.
+        //  Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        //initialize all the distance of vertex to infinity
+        for (int i = 0; i < maze.V(); i++) {
+            distTo[i] = INFINITY;
+        }
+
+        distTo[s] = 0;
+        marked[s] = true;
+        announce();
+
+        queue.add(s);
+
+        while (!queue.isEmpty()) {
+            int v = queue.remove();
+
+            if (v == t) {
+                return;
+            }
+
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    announce();
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] =true;
+                    queue.add(w);
+                }
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
